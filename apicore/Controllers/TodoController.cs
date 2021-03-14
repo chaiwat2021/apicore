e9267ce41d7d533aaf339c453bbcbdfd8baf0dde
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-14 09:46:47 +0700

    Implement DELETE method

--- apicore/Controllers/TodoController.cs
+++ apicore/Controllers/TodoController.cs
@@ -73,5 +73,22 @@ namespace apicore.Controllers
 
             return NoContent();
         }
+
+        // DELETE: api/Todo/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
